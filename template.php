1120f1f231e2c898bac68e57801d4dfafaf8aba2
Phillips Anthony <phillipsanthony2001@gmail.com>
2025-10-04 18:39:17 +0100

    file structure changes

--- template.php
+++
@@ -1,5 +0,0 @@
-<?php include_once __DIR__ . '/includes/css.php'; ?>
-<?php include_once __DIR__ . '/includes/sidebar.php'; ?>
-<!-- paste your code in between -->
-<!-- paste your code in between -->
-<?php include_once __DIR__ . '/includes/js.php'; ?>
